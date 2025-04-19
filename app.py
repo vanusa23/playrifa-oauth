0d344e81d329d63af4bc86103173367dd1dd0b7f
Djuliano Freitas <gvanusa699@gmail.com>
2025-04-19 16:17:24 -0300

    Adicionando rota raiz / para evitar erro 404

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Servidor OAuth PlayRifa está ativo ✅"
+
 @app.route("/oauth")
 def oauth_redirect():
     code = request.args.get("code")
